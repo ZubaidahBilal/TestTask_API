5a7dac4149b4f34c115830749140fb43b2facd9c
Zubaidah Bilal <zubaida.bilal98@gmail.com>
2021-06-02 21:39:46 +0300

    Authentication Login & register

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +19,33 @@ use App\Http\Controllers\CommentController;
 */
 
 //Public routes
-
-//Post routes
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::put('/posts/{id}', [PostController::class, 'update']);
-Route::delete('/posts/{id}', [PostController::class, 'destroy']);
-
-//Comment routes
 Route::get('/comments', [CommentController::class, 'index']);
 Route::get('/comments/{id}', [CommentController::class, 'show']);
-Route::post('/comments', [CommentController::class, 'store']);
-Route::put('/comments/{id}', [CommentController::class, 'update']);
-Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
-
-//Get comment by post
 Route::get('/post-comments/{id}', [PostController::class, 'getCommentsByPost']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::put('/comments/{id}', [CommentController::class, 'update']);
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
+});
+
+
+//Authentication
+
+//Register
+Route::post('/register', [AuthController::class, 'register']);
+
+//Login
+Route::post('/login', [AuthController::class, 'login']);
+
+//Me
+Route::post('/me', [AuthController::class, 'me']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
